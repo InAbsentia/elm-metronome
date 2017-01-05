ae8035135bf5a5331b2400bfc45c56194b81350a
Brent Yoder <brent@bpyoder.com>
2017-01-05 11:15:18 -0700

    Allow user to change BPM

--- Metronome.elm
+++ Metronome.elm
@@ -1,10 +1,13 @@
 module Metronome exposing (..)
 
-import Html exposing (Html)
+import Html exposing (Html, div, input, label)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onInput)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time)
 import AnimationFrame
+
 
 main =
     Html.program
@@ -14,6 +17,7 @@ main =
         , subscriptions = subscriptions
         }
 
+
 type alias Model =
     { angle : Float
     , timeDiff : Time
@@ -22,59 +26,101 @@ type alias Model =
     , speed : Float
     }
 
-init : (Model, Cmd Msg)
+
+init : ( Model, Cmd Msg )
 init =
-    let angle =
+    let
+        angle =
             ((3 * pi) / 2)
+
         timeDiff =
             0
+
         beats =
             4
+
         bpm =
             60
+
         speed =
             calculateSpeed beats bpm
     in
-        ((Model angle timeDiff beats bpm speed), Cmd.none)
+        ( (Model angle timeDiff beats bpm speed), Cmd.none )
+
 
 type Msg
     = TimeUpdate Time
+    | BpmUpdate String
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         TimeUpdate deltaT ->
-            ((updateModel model deltaT), Cmd.none)
+            ( (updatePosition model deltaT), Cmd.none )
+
+        BpmUpdate newBpm ->
+            let
+                intBpm =
+                    case String.toInt newBpm of
+                        Err msg ->
+                            0
+
+                        Ok intBpm ->
+                            intBpm
+            in
+                ( (updateBpm model intBpm), Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     AnimationFrame.diffs TimeUpdate
 
+
 view : Model -> Html Msg
 view model =
-    let newX =
+    let
+        newX =
             toString (50 + 45 * cos model.angle)
 
         newY =
             toString (50 + 45 * sin model.angle)
-
     in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [cx "50", cy "50", r "45", fill "#bada55"] []
-            , circle [cx newX, cy newY, r "3", fill "#666666"] []]
+        div []
+            [ div []
+                [ label []
+                    [ Html.text "BPM"
+                    , input [ type_ "text", value (toString model.bpm), onInput BpmUpdate ] []
+                    ]
+                ]
+            , svg [ viewBox "0 0 100 100", width "300px" ]
+                [ circle [ cx "50", cy "50", r "45", fill "#bada55" ] []
+                , circle [ cx newX, cy newY, r "3", fill "#666666" ] []
+                ]
+            ]
 
-updateModel : Model -> Time -> Model
-updateModel model deltaT =
+
+updatePosition : Model -> Time -> Model
+updatePosition model deltaT =
     { model | timeDiff = deltaT, angle = (newAngle model) }
+
+
+updateBpm : Model -> Int -> Model
+updateBpm model newBpm =
+    { model | bpm = newBpm, speed = (calculateSpeed model.beats newBpm) }
+
 
 newAngle : Model -> Float
 newAngle model =
     model.angle + model.timeDiff * model.speed
 
+
 calculateSpeed : Int -> Int -> Float
 calculateSpeed beats bpm =
-    let rpm =
+    let
+        rpm =
             (1 / (toFloat beats)) * (toFloat bpm)
+
         tau =
             (2 * pi)
     in
